e390bd07dceb7b5d3e797b55b263f78fc5140499
Radek Novak <radek.novak.cze@gmail.com>
2017-12-15 17:46:31 +0100

    turn off tests

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,16 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
     {
-      name      : 'reviewly',
-      script    : 'src/index.js',
-      node_args : '--harmony-async-await',
+      name: 'reviewly',
+      script: 'src/index.js',
+      node_args: '--harmony-async-await',
       env: {
         COMMON_VARIABLE: 'true',
         DEBUG: '*'
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production'
       }
     }
@@ -22,14 +22,15 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'deploy',
-      host : process.env.FEATURE_DEPLOYEMENT_IP,
-      ref  : 'origin/master',
-      repo : 'https://github.com/mycsHQ/reviewly.git',
-      path : '/home/deploy',
-      'post-deploy' : 'npm install && node_modules/.bin/pm2 startOrRestart ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: 'deploy',
+      host: process.env.FEATURE_DEPLOYEMENT_IP,
+      ref: 'origin/github-api',
+      repo: 'https://github.com/mycsHQ/reviewly.git',
+      path: '/home/deploy',
+      'post-deploy':
+        'npm install && node_modules/.bin/pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
 };
